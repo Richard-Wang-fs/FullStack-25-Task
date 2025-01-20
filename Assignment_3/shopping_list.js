9f88cc56f3f3d3e38f05595f1bc1301560c6ce30
Richard <jingpeng.one@gmail.com>
2025-01-21 02:47:47 +0800

    Add comment

--- Assignment_3/shopping_list.js
+++ Assignment_3/shopping_list.js
@@ -1,3 +1,4 @@
+// Define all commodities
 const commoditiesName = ["Apple", "Banana", "Orange", "Strawberry", "Grape", "Pineapple", "Watermelon", "Mango", "Lemon", "Peach", "Plum", "Persimmon", "Dragon Fruit", "Kiwi"];
 
 class commodity {
@@ -21,6 +22,7 @@ class commodityInCart {
 
 let allCommodities = [];
 
+// Record the order in which products are added for recall
 let addedCommodities = [];
 
 let amount = 0;
@@ -40,19 +42,20 @@ class shoppingCart {
     }
     addCommodity (name) {
         if (this.commodities.has(name)) {
-            this.commodities.set(name, this.commodities.get(name)+1); // 如果商品已存在，则增加数量
-        } else 
+            this.commodities.set(name, this.commodities.get(name)+1); 
+        } else // Determine whether the number of product types is greater than 5
             if (this.commodities.size >= 5) {
                 alert("MORE THAN 5 ITEMS!");
             } else {
-                this.commodities.set(name, 1); // 如果商品不存在，则添加新项
+                this.commodities.set(name, 1);
             }
             
         }
     removeCommodity (name) {
         if (this.commodities.has(name)) {
             this.commodities.set(name, this.commodities.get(name)-1);
-            if (this.commodities.get(name) === 0){
+            // If the quantity of the item less than 1, remove it
+            if (this.commodities.get(name) < 1){
                 this.commodities.delete(name);
             }
         } else {
@@ -86,6 +89,7 @@ class shoppingCart {
 
 function generateAllCommodities() {
     let commodities = [...commoditiesName];
+    // Generate unique products and random prices
     for (;commodities.length > 0;) {
         let randomIndex = Math.floor(Math.random() * commodities.length)
         allCommodities.push(new commodity(commodities[randomIndex], Math.floor(Math.random()*9+1)))
@@ -94,9 +98,11 @@ function generateAllCommodities() {
 }
 
 function generateShowcaseList() {
+    // Generate product display list
     const showcaseList = document.getElementById("showcaseList");
     showcaseList.innerHTML = ""; 
 
+    // Differentiate item colors
     allCommodities.forEach((commo, index) => {
         let backgroundColor = 'aqua'
         if (index%2==1){
@@ -114,6 +120,9 @@ function generateShowcaseList() {
         showcaseList.appendChild(itemDiv);
     });
 }
+
+/////////////////////////////////////////////////////////////
+// The following is the operating logic
 
 const cart = new shoppingCart();
 
